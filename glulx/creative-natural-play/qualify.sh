71f2c8c622b875db3678488557c8d9e911a59667
acrinym <acrinym@gmail.com>
2026-08-11 00:48:25 -0400

    Qualify speech through existing parser globals

--- glulx/creative-natural-play/qualify.sh
+++ glulx/creative-natural-play/qualify.sh
@@ -42,9 +42,8 @@ assert '<DO-SL ,WINNER ,SH ,SC>' in g
 assert '<EQUAL? <GET ,P-LEXV ,P-LEXSTART> ,W?SEND>' in g
 assert 'Split sequential actions with a comma or THEN.' in g
 assert '<SETG SIBREAKS ".,\\\"?!">' in g
-assert '<PUT ,OOPS-TABLE ,O-START <+ .PTR ,P-LEXELEN>>' in g
-assert '<PUT ,OOPS-TABLE ,O-LENGTH <- ,P-LEN 1>>' in g
-assert '<PUT ,OOPS-TABLE ,O-END -1>' in g
+assert '<SETG P-ADVERB <+ .PTR ,P-LEXELEN>>' in g
+assert '<SETG P-NUMBER <- ,P-LEN 1>>' in g
 assert ',W?SPEAK' in g
 assert '(SYNONYM ME MYSELF SELF YOU CRETIN)' in globals_
 assert '(DESC "yourself")' in globals_
@@ -62,7 +61,7 @@ assert '<ROUTINE V-YELL-AT ()' in verbs
 assert '<EQUAL? ,PRSO ,ZORK-INTERFACE>' in verbs
 assert '<ROUTINE V-TALK-UNSPECIFIED ()' in verbs
 assert '<ROUTINE PRINT-SPOKEN-INPUT' in verbs
-assert '<EQUAL? <GET ,OOPS-TABLE ,O-END> -1>' in verbs
+assert '<AND ,P-ADVERB <G? ,P-NUMBER 0>>' in verbs
 assert 'is not something you can drink.' in verbs
 assert '<SYNTAX COOK OBJECT = V-KITCHEN-COOK>' in k
 assert '<ROUTINE V-KITCHEN-COOK ()' in k
