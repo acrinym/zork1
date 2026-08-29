21278a5188ec8de1539939102f4a77145e08b19d
acrinym <acrinym@gmail.com>
2026-08-29 05:18:58 -0400

    test: exercise parser-safe furniture route

--- glulx/mundane-objects-spatial-agency/qualify.sh
+++ glulx/mundane-objects-spatial-agency/qualify.sh
@@ -116,7 +116,7 @@ cat > "$BUILD/promise-furniture.txt" <<'EOF_PROMISE'
 tableset
 ask mara to help with kitchen table
 east
-haul kitchen table west
+haul kitchen table westward
 west
 take sack
 quit
